32a8160c4c979c27ee2b40ae18eafd1444bee1f8
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2020-03-14 16:10:44 +0500

    I  make a function group

--- src/Components/App.js
+++ src/Components/App.js
@@ -46,4 +46,21 @@ export default class extends Component {
       </Fragment>
     )
   }
-}       
+}
+// var prop = 'muscles'
+// groupBy(el=>el[prop], exercises);
+function groupBy(getProp, arrOfObj){
+    return arrOfObj.reduce((acc, el, i , arr)=>{
+      let prop = getProp(el);
+      if(prop){
+
+      }
+      acc[prop] = acc[prop]
+      ? [...acc[prop], el]
+      : [el]
+    }, {} )
+}
+
+function isFalse(value){
+  let x = new String(value)
+}
