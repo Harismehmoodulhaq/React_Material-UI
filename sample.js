a447e086090c31ae955ca310ed82e2bfbd7217da
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2020-03-14 18:45:38 +0500

    adding sample.js

--- sample.js
+++ sample.js
@@ -1,17 +1,47 @@
 // var prop = 'muscles'
 // groupBy(el=>el[prop], exercises);
-function groupBy(getProp, arrOfObj){
-    return arrOfObj.reduce((acc, el, i , arr)=>{
-      let prop = getProp(el);
-      if(prop){
 
-      }
-      acc[prop] = acc[prop]
-      ? [...acc[prop], el]
-      : [el]
-    }, {} )
+
+function groupBy(getPropFn, arrOfObj) {
+    // let group={};
+
+    // arrOfgroup.forEach(el=> {
+    //   let mem = getPropFn(el)
+    //   if(is(mam)) {
+    //     group[mem] = group[mem] ? [...group[mem], el] : [el]
+    //   }
+
+    // })
+    // return group;
+    return arrOfObj.reduce((acc, el, i, arr) => {
+        let prop = getPropFn(el);
+        debugger;
+        if (prop) {
+
+        }
+        acc[prop] = acc[prop]
+            ? [...acc[prop], el]
+            : [el]
+        return acc;
+    }, {})
+
 }
 
-function isFalse(value){
-  let x = new String(value)
+function ObjTuple(obj) {
+    return Object.entries(obj);
+
 }
+
+function is(val) {
+    return (typeof val === 'undefined') ? false : true;
+}
+
+if (exercises[muscles]) {
+    exercises[muscles] = [...exercises[muscles], exercise]
+    debugger;
+} else {
+    exercises[muscles] = [exercise]
+}
+
+return ObjTuple(groupBy(el => el.muscles, this.state.exercises))
+return Object.entries(groupBy(el => el.muscles, this.state.exercises))
--- src/Components/App.js
+++ src/Components/App.js
@@ -47,20 +47,3 @@ export default class extends Component {
     )
   }
 }
-// var prop = 'muscles'
-// groupBy(el=>el[prop], exercises);
-function groupBy(getProp, arrOfObj){
-    return arrOfObj.reduce((acc, el, i , arr)=>{
-      let prop = getProp(el);
-      if(prop){
-
-      }
-      acc[prop] = acc[prop]
-      ? [...acc[prop], el]
-      : [el]
-    }, {} )
-}
-
-function isFalse(value){
-  let x = new String(value)
-}
