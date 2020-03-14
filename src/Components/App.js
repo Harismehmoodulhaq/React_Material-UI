b1b79e2df2fd858a842905124218cb2de7c56d5f
Harismehmoodulhaq <hsmeh.dev@gmail.com>
2020-03-14 07:47:57 +0500

    removed App.test.js

--- src/Components/App.js
+++ src/Components/App.js
@@ -2,15 +2,7 @@ import React, { Component, Fragment } from "react"
 import { Header, Footer } from "./Layouts"
 import Exercises from "./Exercises"
 import { muscles, exercises } from "../store.js"
-// import { Object } from "prop-types"
-// import { getThemeProps } from "@material-ui/styles";
-// function App() {
-//   return (
-//     <div>
-//       <h1>hi</h1>
-//     </div>
-//   );getThemeProps
-// }
+
 
 export default class extends Component {
   state = {
@@ -22,16 +14,16 @@ export default class extends Component {
     return Object.entries(
       this.state.exercises.reduce((exercises, exercise) => {
         const { muscles } = exercise
-        // debugger;
+       
         exercises[muscles] = exercises[muscles]
           ? [...exercises[muscles], exercise]
           : [exercise]
-        // debugger;
+       
         return exercises
       }, {})
     )
   }
-  // props;
+ 
   onSelect = (event, index) => {
     debugger;
     this.setState({muscleIndex: index})
